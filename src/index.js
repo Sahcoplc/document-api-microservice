a3f682272be9bed05a411147a69640f1155c8aec
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2025-01-10 02:54:40 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { updateCertificateStatus } from "./helpers/fetch.js"
 
 const { PORT } = process.env
 
-cron.schedule('02 40 01 * * *', async () => {
+cron.schedule('03 01 01 * * *', async () => {
     console.log('Running cron job')
     await updateCertificateStatus()
 })
